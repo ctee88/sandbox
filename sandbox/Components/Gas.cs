82a4c70601e0537c80a889b2e09318749116d3e0
cte88 <camerontee88@gmail.com>
2024-05-28 18:19:52 +0100

    Modified Gas movement - should move left and right like Liquids

--- sandbox/Components/Gas.cs
+++ sandbox/Components/Gas.cs
@@ -16,7 +16,8 @@ namespace sandbox.Components
             if (ElementMatrix.IsWithinBounds(x, y - 1) && (ElementMatrix.IsEmptyCell(x, y - 1)))
             {
                 //Only accounts for empty cells - might get trapped in liquid and movable solids
-                ElementMatrix.elements[x, y] = null;
+                //ElementMatrix.elements[x, y] = null;
+                ElementMatrix.elements[x, y] = ElementMatrix.elements[x, y - 1];
                 ElementMatrix.elements[x, y - 1] = element;
 
                 index[0] = x;
@@ -26,9 +27,10 @@ namespace sandbox.Components
             }
 
             //Above left
-            if (ElementMatrix.IsWithinBounds(x - 1, y - 1) && (ElementMatrix.IsEmptyCell(x - 1, y - 1)))
+            else if (ElementMatrix.IsWithinBounds(x - 1, y - 1) && (ElementMatrix.IsEmptyCell(x - 1, y - 1)))
             {
-                ElementMatrix.elements[x, y] = null;
+                //ElementMatrix.elements[x, y] = null;
+                ElementMatrix.elements[x, y] = ElementMatrix.elements[x - 1, y - 1];
                 ElementMatrix.elements[x - 1, y - 1] = element;
 
                 index[0] = x - 1;
@@ -38,13 +40,66 @@ namespace sandbox.Components
             }
 
             //Above right
-            if (ElementMatrix.IsWithinBounds(x + 1, y - 1) && (ElementMatrix.IsEmptyCell(x + 1, y - 1)))
+            else if (ElementMatrix.IsWithinBounds(x + 1, y - 1) && (ElementMatrix.IsEmptyCell(x + 1, y - 1)))
             {
-                ElementMatrix.elements[x, y] = null;
+                //ElementMatrix.elements[x, y] = null;
+                ElementMatrix.elements[x, y] = ElementMatrix.elements[x + 1, y - 1];
                 ElementMatrix.elements[x + 1, y - 1] = element;
 
                 index[0] = x + 1;
                 index[1] = y - 1;
+
+                return index;
+            }
+
+            //Check these after the 3 cells below are occupied
+            //There is 100% a way to rewrite these if statements, this method is getting quite ugly 
+            //as there is a lot of repeated code
+            //Left and Right both empty and within bounds
+            else if ((ElementMatrix.IsWithinBounds(x - 1, y) && ElementMatrix.CanMoveThrough(x - 1, y)) &&
+                (ElementMatrix.IsWithinBounds(x + 1, y) && ElementMatrix.CanMoveThrough(x + 1, y)))
+            {
+                if (leftOrRight == true)
+                {
+                    ElementMatrix.elements[x, y] = ElementMatrix.elements[x - 1, y];
+                    ElementMatrix.elements[x - 1, y] = element;
+                    //ElementMatrix.elements[x, y] = null;
+                    index[0] = x - 1;
+                    index[1] = y;
+
+                    return index;
+                }
+                else //Is it better to be explicit here? leftOrRight == false
+                {
+                    ElementMatrix.elements[x, y] = ElementMatrix.elements[x + 1, y];
+                    ElementMatrix.elements[x + 1, y] = element;
+                    //ElementMatrix.elements[x, y] = null;
+                    index[0] = x + 1;
+                    index[1] = y;
+
+                    return index;
+                }
+            }
+            //Left
+            else if (ElementMatrix.IsWithinBounds(x - 1, y) && ElementMatrix.CanMoveThrough(x - 1, y))
+            {
+                ElementMatrix.elements[x, y] = ElementMatrix.elements[x - 1, y];
+                ElementMatrix.elements[x - 1, y] = element;
+                //ElementMatrix.elements[x, y] = null;
+                index[0] = x - 1;
+                index[1] = y;
+
+                return index;
+            }
+
+            //Right
+            else if (ElementMatrix.IsWithinBounds(x + 1, y) && ElementMatrix.CanMoveThrough(x + 1, y))
+            {
+                ElementMatrix.elements[x, y] = ElementMatrix.elements[x + 1, y];
+                ElementMatrix.elements[x + 1, y] = element;
+                //ElementMatrix.elements[x, y] = null;
+                index[0] = x + 1;
+                index[1] = y;
 
                 return index;
             }
